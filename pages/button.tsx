fd3f7a704466ebc761901ff0872087bb52892ad9
shunya-kitajima <42736897+shunya-kitajima@users.noreply.github.com>
2022-10-11 18:20:33 +0900

    change styles

--- pages/button.tsx
+++ pages/button.tsx
@@ -1,12 +1,26 @@
 import React from 'react'
 import { Button, Group } from '@mantine/core'
+import { BrandGithub, ThreeDCubeSphere } from 'tabler-icons-react'
 import { Layout } from '../components/Layout'
 
 const ButtonDemo: React.FC = () => {
   return (
     <Layout title="Button">
-      <Group>
-        <Button>Press</Button>
+      <Group direction="column" position="center">
+        <Button
+          classNames={{
+            leftIcon: 'text-pink-500 h-6 w-6',
+            rightIcon: 'text-orange-500 h-6 w-6',
+          }}
+          color="cyan"
+          radius="lg"
+          uppercase
+          leftIcon={<BrandGithub />}
+          rightIcon={<ThreeDCubeSphere />}
+        >
+          Press
+        </Button>
+        <Button mt="md">Click</Button>
       </Group>
     </Layout>
   )
