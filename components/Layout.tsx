c93bf140204122226d5515dd45bf6e04e7819d76
shunya-kitajima <42736897+shunya-kitajima@users.noreply.github.com>
2022-10-11 16:23:32 +0900

    create Layout component

---
+++ components/Layout.tsx
@@ -0,0 +1,22 @@
+import React, { ReactNode } from 'react'
+import Head from 'next/head'
+
+type Props = {
+  title: string
+  children: ReactNode
+}
+
+export const Layout: React.FC<Props> = ({ title = 'Mantine', children }) => {
+  return (
+    <div className="flex min-h-screen">
+      <Head>
+        <title>{title}</title>
+      </Head>
+      <header></header>
+      <main className="flex flex-1 flex-col justify-center p-4">
+        {children}
+      </main>
+      <footer></footer>
+    </div>
+  )
+}
