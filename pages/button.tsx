80e09b316e726ab1dd9081233a2cfe6ad9f7ccc8
shunya-kitajima <42736897+shunya-kitajima@users.noreply.github.com>
2022-10-11 16:51:43 +0900

    import Button

--- pages/button.tsx
+++ pages/button.tsx
@@ -1,8 +1,15 @@
 import React from 'react'
+import { Button, Group } from '@mantine/core'
 import { Layout } from '../components/Layout'
 
-const ButtonDemo = () => {
-  return <Layout title="Button">button</Layout>
+const ButtonDemo: React.FC = () => {
+  return (
+    <Layout title="Button">
+      <Group>
+        <Button>Press</Button>
+      </Group>
+    </Layout>
+  )
 }
 
 export default ButtonDemo
