ff6cb72c2e662bff2039f7eb5ef8a7038c9ae533
shunya-kitajima <42736897+shunya-kitajima@users.noreply.github.com>
2022-10-12 14:37:52 +0900

    create grid.cols

--- pages/grid.tsx
+++ pages/grid.tsx
@@ -14,6 +14,115 @@ const GridDemo: React.FC = () => {
         >
           1
         </Grid.Col>
+        <Grid.Col
+          className="bg-green-500 text-center font-bold text-gray-100"
+          span={4}
+        >
+          2
+        </Grid.Col>
+        <Grid.Col
+          className="bg-pink-500 text-center font-bold text-gray-100"
+          span={4}
+        >
+          3
+        </Grid.Col>
+      </Grid>
+      <Grid my="md" grow>
+        <Grid.Col
+          className="bg-blue-500 text-center font-bold text-gray-100"
+          span={6}
+        >
+          4
+        </Grid.Col>
+        <Grid.Col
+          className="bg-green-500 text-center font-bold text-gray-100"
+          span={6}
+        >
+          5
+        </Grid.Col>
+        <Grid.Col
+          className="bg-pink-500 text-center font-bold text-gray-100"
+          span={3}
+        >
+          6
+        </Grid.Col>
+        <Grid.Col
+          className="bg-orange-500 text-center font-bold text-gray-100"
+          span={3}
+        >
+          7
+        </Grid.Col>
+      </Grid>
+      <Grid mt="md">
+        <Grid.Col
+          className="bg-indigo-500 text-center font-bold text-gray-100"
+          span={3}
+        >
+          8
+        </Grid.Col>
+        <Grid.Col
+          className="bg-green-500 text-center font-bold text-gray-100"
+          span={3}
+        >
+          9
+        </Grid.Col>
+        <Grid.Col
+          className="bg-pink-500 text-center font-bold text-gray-100"
+          span={3}
+          offset={3}
+        >
+          10
+        </Grid.Col>
+      </Grid>
+      <Grid mt="md" justify="center" align="center">
+        <Grid.Col
+          className="h-24 bg-blue-500 text-center font-bold text-gray-100"
+          span={3}
+        >
+          11
+        </Grid.Col>
+        <Grid.Col
+          className="h-36 bg-green-500 text-center font-bold text-gray-100"
+          span={3}
+        >
+          12
+        </Grid.Col>
+        <Grid.Col
+          className="h-12 bg-pink-500 text-center font-bold text-gray-100"
+          span={3}
+        >
+          13
+        </Grid.Col>
+      </Grid>
+      <Grid mt="md">
+        <Grid.Col
+          className="bg-blue-500 text-center font-bold text-gray-100"
+          md={6}
+          lg={3}
+        >
+          14
+        </Grid.Col>
+        <Grid.Col
+          className="bg-green-500 text-center font-bold text-gray-100"
+          md={6}
+          lg={3}
+        >
+          15
+        </Grid.Col>
+        <Grid.Col
+          className="bg-pink-500 text-center font-bold text-gray-100"
+          md={6}
+          lg={3}
+        >
+          16
+        </Grid.Col>
+        <Grid.Col
+          className="bg-orange-500 text-center font-bold text-gray-100"
+          md={6}
+          lg={3}
+        >
+          17
+        </Grid.Col>
       </Grid>
       <Center>
         <Link href="/">
