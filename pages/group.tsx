b2af6b90e92c3294edb7e05c1805d1defd7b729f
shunya-kitajima <42736897+shunya-kitajima@users.noreply.github.com>
2022-10-12 10:25:49 +0900

    create groups

--- pages/group.tsx
+++ pages/group.tsx
@@ -7,11 +7,32 @@ import { Layout } from '../components/Layout'
 const GroupDemo: React.FC = () => {
   return (
     <Layout title="Group">
-      <Group my="md">
+      <Group my="md" grow>
         <Button color="indigo">1</Button>
         <Button color="teal">2</Button>
         <Button color="orange">3</Button>
       </Group>
+      <Group my="md" direction="column" position="right">
+        <Button color="indigo">1</Button>
+        <Button color="teal">2</Button>
+        <Button color="orange">3</Button>
+      </Group>
+      <Group my="md" align="center">
+        <Button color="indigo" className="h-10">
+          1
+        </Button>
+        <Button color="teal" className="h-20">
+          2
+        </Button>
+        <Button color="orange" className="h-32">
+          3
+        </Button>
+      </Group>
+      <Center>
+        <Link href="/">
+          <ArrowUturnLeftIcon className="mt-4 h-6 w-6 cursor-pointer text-gray-300" />
+        </Link>
+      </Center>
     </Layout>
   )
 }
