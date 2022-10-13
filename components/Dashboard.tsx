82164bdbc192c47e1026d407d90e943374c4a262
shunya-kitajima <42736897+shunya-kitajima@users.noreply.github.com>
2022-10-13 11:20:40 +0900

    import modules

---
+++ components/Dashboard.tsx
@@ -0,0 +1,3 @@
+import { ShieldCheckIcon } from '@heroicons/react/24/solid'
+import { ArrowLeftOnRectangleIcon } from '@heroicons/react/24/outline'
+import { ActionIcon, Center, Menu } from '@mantine/core'
