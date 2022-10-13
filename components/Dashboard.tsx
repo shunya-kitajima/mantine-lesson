b696894d5374386556b3258a9ef79793bae7c8c9
shunya-kitajima <42736897+shunya-kitajima@users.noreply.github.com>
2022-10-13 11:27:31 +0900

    create signOut

--- components/Dashboard.tsx
+++ components/Dashboard.tsx
@@ -1,3 +1,25 @@
+import React from 'react'
 import { ShieldCheckIcon } from '@heroicons/react/24/solid'
 import { ArrowLeftOnRectangleIcon } from '@heroicons/react/24/outline'
 import { ActionIcon, Center, Menu } from '@mantine/core'
+import { supabase } from '../utils/supabase'
+import { Layout } from './Layout'
+
+export const DashBoard: React.FC = () => {
+  const signOut = () => {
+    supabase.auth.signOut()
+  }
+
+  return (
+    <Layout title="DashBoard">
+      <Center>
+        <ShieldCheckIcon className="mb-4 h-14 w-14 text-teal-500" />
+      </Center>
+      <Center>
+        <ActionIcon my="md" size="lg" onClick={signOut}>
+          <ArrowLeftOnRectangleIcon />
+        </ActionIcon>
+      </Center>
+    </Layout>
+  )
+}
