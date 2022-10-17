0fddf2db66409e58ddc178d84b0558b5ad02b963
shunya-kitajima <42736897+shunya-kitajima@users.noreply.github.com>
2022-10-17 18:32:58 +0900

    import modules

---
+++ components/FetchTodos.tsx
@@ -0,0 +1,8 @@
+import React from 'react'
+import { ExclamationCircleIcon } from '@heroicons/react/24/solid'
+import { Loader, Text, Alert, Center } from '@mantine/core'
+import { useQueryTodos } from '../hooks/useQueryTodos'
+
+export const FetchTodos: React.FC = () => {
+  return <div>FetchTodos</div>
+}
