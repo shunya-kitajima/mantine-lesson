c8dc82818723f08be08969297aa2591b9a07f78c
shunya-kitajima <42736897+shunya-kitajima@users.noreply.github.com>
2022-10-17 17:00:59 +0900

    display radiogroup

--- pages/multi-select.tsx
+++ pages/multi-select.tsx
@@ -14,8 +14,27 @@ import {
 import { Layout } from '../components/Layout'
 
 const MultiSelectDemo: React.FC = () => {
+  const [radioValue, setRadioValue] = useState('react')
+  const [selectValue, setSelectValue] = useState<string[]>([])
+
   return (
     <Layout title="Multi-Select">
+      <Container>
+        <Text>🚀 What is your most favorite frontend library ?</Text>
+        <Center>
+          <RadioGroup
+            my="lg"
+            value={radioValue}
+            onChange={setRadioValue}
+            required
+          >
+            <Radio value="react" label="React" />
+            <Radio value="svelte" label="Svelte" />
+            <Radio value="angular" label="Angular" />
+            <Radio value="vue" label="Vue" />
+          </RadioGroup>
+        </Center>
+      </Container>
       <Center>
         <Link href="/">
           <ArrowUturnLeftIcon className="mt-4 h-6 w-6 cursor-pointer text-gray-300" />
