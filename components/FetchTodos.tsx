9b18f7144c81524f064521a5b007fe1082fb9380
shunya-kitajima <42736897+shunya-kitajima@users.noreply.github.com>
2022-10-17 18:36:58 +0900

    create case of loading and error

--- components/FetchTodos.tsx
+++ components/FetchTodos.tsx
@@ -4,5 +4,28 @@ import { Loader, Text, Alert, Center } from '@mantine/core'
 import { useQueryTodos } from '../hooks/useQueryTodos'
 
 export const FetchTodos: React.FC = () => {
+  const { data, status } = useQueryTodos()
+
+  if (status === 'loading')
+    return (
+      <Center>
+        <Loader color="indigo" size="xl" />
+      </Center>
+    )
+
+  if (status === 'error')
+    return (
+      <Center>
+        <Alert
+          icon={<ExclamationCircleIcon />}
+          title="Fetch Error!"
+          color="red"
+          radius="md"
+        >
+          Something wrong happend !
+        </Alert>
+      </Center>
+    )
+
   return <div>FetchTodos</div>
 }
