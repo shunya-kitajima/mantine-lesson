59f33c6f4f2bfa0cf857a4eae31021c2ba95ecb7
shunya-kitajima <42736897+shunya-kitajima@users.noreply.github.com>
2022-10-17 18:45:16 +0900

    import modules

---
+++ pages/fetch-api.tsx
@@ -0,0 +1,23 @@
+import React from 'react'
+import Link from 'next/link'
+import { ArrowUturnLeftIcon } from '@heroicons/react/20/solid'
+import { Center } from '@mantine/core'
+import { FetchTodos } from '../components/FetchTodos'
+import { Layout } from '../components/Layout'
+
+const FetchData: React.FC = () => {
+  return (
+    <Layout title="FetchData">
+      <Center>
+        <FetchTodos />
+      </Center>
+      <Center>
+        <Link href="/">
+          <ArrowUturnLeftIcon className="mt-4 h-6 w-6 cursor-pointer text-gray-300" />
+        </Link>
+      </Center>
+    </Layout>
+  )
+}
+
+export default FetchData
