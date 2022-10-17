9d028388a4f1d3f59f32e7c22ba952dd63dbb3a0
shunya-kitajima <42736897+shunya-kitajima@users.noreply.github.com>
2022-10-17 16:23:13 +0900

    display item for link of button

--- components/Dashboard.tsx
+++ components/Dashboard.tsx
@@ -2,6 +2,8 @@ import React from 'react'
 import { ShieldCheckIcon } from '@heroicons/react/24/solid'
 import { ArrowRightOnRectangleIcon } from '@heroicons/react/24/outline'
 import { ActionIcon, Center, Menu } from '@mantine/core'
+import { NextLink } from '@mantine/next'
+import { Settings } from 'tabler-icons-react'
 import { supabase } from '../utils/supabase'
 import { Layout } from './Layout'
 
@@ -17,6 +19,13 @@ export const DashBoard: React.FC = () => {
       </Center>
       <Menu trigger="hover" size="xl">
         <Menu.Label> UI Component</Menu.Label>
+        <Menu.Item
+          icon={<Settings size={16} />}
+          component={NextLink}
+          href="/button"
+        >
+          Button
+        </Menu.Item>
       </Menu>
       <Center>
         <ActionIcon my="md" size="lg" onClick={signOut}>
