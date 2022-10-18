8916ac80133320fcbcbfbb52c018e2fcc1fe0cfb
shunya-kitajima <42736897+shunya-kitajima@users.noreply.github.com>
2022-10-18 16:01:25 +0900

    display AuthenForm compoent and link of home

--- pages/modal.tsx
+++ pages/modal.tsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useState } from 'react'
 import Link from 'next/link'
 import { ArrowUturnLeftIcon } from '@heroicons/react/20/solid'
 import { Modal, Button, Group } from '@mantine/core'
@@ -6,7 +6,26 @@ import { AuthenForm } from '../components/AuthenForm'
 import { Layout } from '../components/Layout'
 
 const Modaldemo = () => {
-  return <div></div>
+  const [opened, setOpened] = useState(false)
+
+  return (
+    <Layout title="Modal">
+      <Modal
+        title="Authorization Form"
+        centered
+        opened={opened}
+        onClose={() => setOpened(false)}
+      >
+        <AuthenForm />
+      </Modal>
+      <Group direction="column" position="center">
+        <Button onClick={() => setOpened(true)}>Open Modal</Button>
+        <Link href="/">
+          <ArrowUturnLeftIcon className="mt-4 h-6 w-6 cursor-pointer text-gray-300" />
+        </Link>
+      </Group>
+    </Layout>
+  )
 }
 
 export default Modaldemo
