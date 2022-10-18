4d925c5513851fbb793e8d490a4f58cff659b745
shunya-kitajima <42736897+shunya-kitajima@users.noreply.github.com>
2022-10-18 16:12:49 +0900

    modify

--- components/AuthenForm.tsx
+++ components/AuthenForm.tsx
@@ -61,7 +61,7 @@ export const AuthenForm: React.FC = () => {
         {isRegister && (
           <Group grow>
             <TextInput
-              data-autoFocus
+              autoFocus
               placeholder="Your first name"
               label="First name"
               {...form.getInputProps('firstName')}
