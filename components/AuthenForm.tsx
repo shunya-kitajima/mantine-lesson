d3b4422ad6781b33b25db0c98e9318ae7791e513
shunya-kitajima <42736897+shunya-kitajima@users.noreply.github.com>
2022-10-18 15:24:22 +0900

    create schema

--- components/AuthenForm.tsx
+++ components/AuthenForm.tsx
@@ -13,6 +13,19 @@ import {
 } from '@mantine/core'
 import { AuthForm } from '../types'
 
+const schema = Yup.object().shape({
+  firstName: Yup.string().required('No first name provided'),
+  lastName: Yup.string().required('No last name provided'),
+  email: Yup.string().email('Invalid email').required('No email provided'),
+  password: Yup.string()
+    .min(8, 'Password should be min 8 chars')
+    .required('No password provided'),
+  confirmPassword: Yup.string()
+    .oneOf([Yup.ref('password')], 'Password does not match')
+    .required(),
+  termsOfService: Yup.boolean().required(),
+})
+
 export const AuthenForm: React.FC = () => {
   return <div>AuthenForm</div>
 }
