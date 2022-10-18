b2c018fff774ab97a009fb8d3066689e2543506c
shunya-kitajima <42736897+shunya-kitajima@users.noreply.github.com>
2022-10-18 15:56:00 +0900

    import modules

---
+++ pages/modal.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import Link from 'next/link'
+import { ArrowUturnLeftIcon } from '@heroicons/react/20/solid'
+import { Modal, Button, Group } from '@mantine/core'
+import { AuthenForm } from '../components/AuthenForm'
+import { Layout } from '../components/Layout'
+
+const Modaldemo = () => {
+  return <div></div>
+}
+
+export default Modaldemo
