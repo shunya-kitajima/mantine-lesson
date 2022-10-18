78ff54a8385c803f55ecdfd5da15dd95feb32526
shunya-kitajima <42736897+shunya-kitajima@users.noreply.github.com>
2022-10-18 14:59:00 +0900

    display dialogs

--- pages/dialog.tsx
+++ pages/dialog.tsx
@@ -15,11 +15,27 @@ const DialogDemo: React.FC = () => {
 
   return (
     <Layout title="Dialog">
-      <Center>
+      <Dialog
+        opened={opened}
+        withCloseButton
+        onClose={() => setOpened(false)}
+        size="lg"
+        radius="md"
+      >
+        <Text size="sm" mb="md">
+          Subscribe to email newsletter
+        </Text>
+        <Group>
+          <TextInput placeholder="xxx@gmail.com" className="flex-1" />
+          <Button onClick={() => setOpened(false)}>Subscribe</Button>
+        </Group>
+      </Dialog>
+      <Group direction="column" position="center">
+        <Button onClick={() => setOpened(!opened)}>Toggle dialog</Button>
         <Link href="/">
           <ArrowUturnLeftIcon className="mt-4 h-6 w-6 cursor-pointer text-gray-300" />
         </Link>
-      </Center>
+      </Group>
     </Layout>
   )
 }
