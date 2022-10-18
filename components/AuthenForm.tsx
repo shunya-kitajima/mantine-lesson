256dd4c13edad27c7bebaca2dff78772040edc48
shunya-kitajima <42736897+shunya-kitajima@users.noreply.github.com>
2022-10-18 15:12:22 +0900

    import modules

---
+++ components/AuthenForm.tsx
@@ -0,0 +1,18 @@
+import React, { useState } from 'react'
+import * as Yup from 'yup'
+import { useForm, yupResolver } from '@mantine/form'
+import {
+  TextInput,
+  PasswordInput,
+  Group,
+  Checkbox,
+  Button,
+  Paper,
+  LoadingOverlay,
+  Anchor,
+} from '@mantine/core'
+import { AuthForm } from '../types'
+
+export const AuthenForm: React.FC = () => {
+  return <div>AuthenForm</div>
+}
