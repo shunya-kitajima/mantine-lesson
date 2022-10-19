059d02891de709ccc8ed131030653d6609db8bf2
shunya-kitajima <42736897+shunya-kitajima@users.noreply.github.com>
2022-10-19 15:15:26 +0900

    import modules

--- pages/card.tsx
+++ pages/card.tsx
@@ -1,5 +1,9 @@
 import React, { ChangeEvent, useState } from 'react'
 import Link from 'next/link'
+import { useQueryClient } from 'react-query'
+import * as Yup from 'yup'
+import { ArrowUturnLeftIcon, CameraIcon } from '@heroicons/react/20/solid'
+import { useForm, yupResolver } from '@mantine/form'
 import {
   Grid,
   Container,
@@ -10,7 +14,11 @@ import {
   Textarea,
   Select,
 } from '@mantine/core'
+import { Layout } from '../components/Layout'
+import { CustomCard } from '../components/CustomCard'
 import { useQueryPosts } from '../hooks/useQueryPosts'
+import { supabase } from '../utils/supabase'
+import { Post } from '../types'
 
 const PostList: React.FC = () => {
   return <div></div>
