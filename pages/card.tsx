eab2d59d4d5055e821089929cdcccd1c4fa12125
shunya-kitajima <42736897+shunya-kitajima@users.noreply.github.com>
2022-10-19 15:12:37 +0900

    import modules

---
+++ pages/card.tsx
@@ -0,0 +1,19 @@
+import React, { ChangeEvent, useState } from 'react'
+import Link from 'next/link'
+import {
+  Grid,
+  Container,
+  Center,
+  Button,
+  TextInput,
+  Loader,
+  Textarea,
+  Select,
+} from '@mantine/core'
+import { useQueryPosts } from '../hooks/useQueryPosts'
+
+const PostList: React.FC = () => {
+  return <div></div>
+}
+
+export default PostList
