b12f4fbedbae28e0b2ce9fb8f39bfdae74fa5498
shunya-kitajima <42736897+shunya-kitajima@users.noreply.github.com>
2022-10-19 15:55:49 +0900

    display link of home

--- pages/card.tsx
+++ pages/card.tsx
@@ -131,6 +131,11 @@ const PostList: React.FC = () => {
           </Grid.Col>
         ))}
       </Grid>
+      <Center>
+        <Link href="/">
+          <ArrowUturnLeftIcon className="mt-4 h-6 w-6 cursor-pointer text-gray-300" />
+        </Link>
+      </Center>
     </Layout>
   )
 }
