448f742230d5d328e4fc8577105808ea78d84b2b
shunya-kitajima <42736897+shunya-kitajima@users.noreply.github.com>
2022-10-19 09:45:42 +0900

    import modules

---
+++ pages/avatar.tsx
@@ -0,0 +1,16 @@
+import React, {ChangeEvent, useState, useEffect} from 'react'
+import Link from 'next/link'
+import { ArrowUturnLeftIcon, CameraIcon } from '@heroicons/react/20/solid'
+import {Avatar, Button, Group, Indicator, Loader} from '@mantine/core'
+import { supabase } from '../utils/supabase'
+import { Layout } from '../components/Layout'
+
+const AvatarDemo: React.FC = () => {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default AvatarDemo
