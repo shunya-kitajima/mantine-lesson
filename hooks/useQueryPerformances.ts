94c42c15ce23b4196ef5bee24775a2084e264168
shunya-kitajima <42736897+shunya-kitajima@users.noreply.github.com>
2022-10-20 15:03:49 +0900

    create useEffect

--- hooks/useQueryPerformances.ts
+++ hooks/useQueryPerformances.ts
@@ -5,12 +5,32 @@ import { supabase } from '../utils/supabase'
 import { Performance } from '../types'
 
 export const useQueryPerformances = () => {
-  const getPerformances = async () => {
-    const { data, error } = await supabase
-      .from('performances')
-      .select('*')
-      .order('created_at', { ascending: true })
-  }
+  const queryClient = useQueryClient()
 
-  return {}
+  useEffect(() => {
+    const subsc = supabase
+      .from('performances')
+      .on('UPDATE', (payload: SupabaseRealtimePayload<Performance>) => {
+        queryClient.setQueryData(['performance'], {
+          id: payload.new.id,
+          created_at: payload.new.created_at,
+          efficiency: payload.new.efficiency,
+          comfort: payload.new.comfort,
+          luck: payload.new.luck,
+          durability: payload.new.durability,
+          user_id: payload.new.user_id,
+          level: payload.new.level,
+        })
+      })
+      .on('DELETE', (payload: SupabaseRealtimePayload<Performance>) => {
+        queryClient.setQueryData(['performance'], null)
+      })
+      .subscribe()
+    const removeSubscription = async () => {
+      await supabase.removeSubscription(subsc)
+    }
+    return () => {
+      removeSubscription()
+    }
+  }, [])
 }
