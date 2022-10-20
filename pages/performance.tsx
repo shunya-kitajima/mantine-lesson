692ad0b18e329b1838d9d3e6f8b93319b1f9eba9
shunya-kitajima <42736897+shunya-kitajima@users.noreply.github.com>
2022-10-20 15:43:08 +0900

    create states and useEffect

--- pages/performance.tsx
+++ pages/performance.tsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 import Link from 'next/link'
 import { ArrowUturnLeftIcon } from '@heroicons/react/20/solid'
 import {
@@ -21,8 +21,23 @@ import { supabase } from '../utils/supabase'
 import { useQueryPerformances } from '../hooks/useQueryPerformances'
 import { Layout } from '../components/Layout'
 
-const PerformanceDemo = () => {
-  return <div></div>
+const PerformanceDemo: React.FC = () => {
+  const [efficiency, setEfficiency] = useState<number | undefined>(0)
+  const [comport, setComfort] = useState<number | undefined>(0)
+  const [luck, setLuck] = useState<number | undefined>(0)
+  const [durability, setDurability] = useState<number | undefined>(0)
+  const [level, setLevel] = useState<number | undefined>(0)
+  const { data } = useQueryPerformances()
+
+  useEffect(() => {
+    setEfficiency(data?.efficiency)
+    setComfort(data?.comfort)
+    setLuck(data?.luck)
+    setDurability(data?.durability)
+    setLevel(data?.level)
+  }, [data])
+
+  return <Layout title="Performance"></Layout>
 }
 
 export default PerformanceDemo
