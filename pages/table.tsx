90eb00201f5293d50a7f0a93f8dc0bd78ac2986e
shunya-kitajima <42736897+shunya-kitajima@users.noreply.github.com>
2022-10-21 13:22:09 +0900

    display link of home

--- pages/table.tsx
+++ pages/table.tsx
@@ -1,11 +1,22 @@
 import React from 'react'
+import Link from 'next/link'
 import { Table, Container, Loader, Center } from '@mantine/core'
 import { ArrowUturnLeftIcon } from '@heroicons/react/20/solid'
 import { useQueryTodos } from '../hooks/useQueryTodos'
 import { Layout } from '../components/Layout'
 
-const table: React.FC = () => {
-  return <div></div>
+const TableDemo: React.FC = () => {
+  const { data } = useQueryTodos()
+
+  return (
+    <Layout title="Table">
+      <Center>
+        <Link href="/">
+          <ArrowUturnLeftIcon className="mt-4 h-6 w-6 cursor-pointer text-gray-300" />
+        </Link>
+      </Center>
+    </Layout>
+  )
 }
 
-export default table
+export default TableDemo
