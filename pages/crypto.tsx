4edfdd2ae4b68e1da1f467d025fe5cc9bd95f1eb
shunya-kitajima <42736897+shunya-kitajima@users.noreply.github.com>
2022-10-21 13:41:26 +0900

    import modules

---
+++ pages/crypto.tsx
@@ -0,0 +1,14 @@
+import React, { useState, useEffect } from 'react'
+import Link from 'next/link'
+import axios from 'axios'
+import { ArrowUturnLeftIcon, BoltIcon } from '@heroicons/react/20/solid'
+import { Text, Group } from '@mantine/core'
+import { DatePicker } from '@mantine/dates'
+import { getDate, getMonth, getYear } from 'date-fns'
+import { Layout } from '../components/Layout'
+
+const CryptoDeom: React.FC = () => {
+  return <Layout title="Crypto"></Layout>
+}
+
+export default CryptoDeom
