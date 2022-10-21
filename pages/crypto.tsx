4fbaaff113f1ac47797d99b43fa3d53ff4ddea8f
shunya-kitajima <42736897+shunya-kitajima@users.noreply.github.com>
2022-10-21 14:06:36 +0900

    change for async function in useEffect

--- pages/crypto.tsx
+++ pages/crypto.tsx
@@ -2,7 +2,7 @@ import React, { useState, useEffect } from 'react'
 import Link from 'next/link'
 import axios from 'axios'
 import { ArrowUturnLeftIcon, BoltIcon } from '@heroicons/react/20/solid'
-import { Text, Group } from '@mantine/core'
+import { Text, Group, Loader } from '@mantine/core'
 import { DatePicker } from '@mantine/dates'
 import { getDate, getMonth, getYear } from 'date-fns'
 import { Layout } from '../components/Layout'
@@ -17,31 +17,35 @@ const CryptoDeom: React.FC = () => {
   const [date, setDate] = useState<Date | null>(new Date())
   const [btc, setBtc] = useState<Coin | null>(null)
   const [eth, setEth] = useState<Coin | null>(null)
+  const [isLoading, setIsLoading] = useState(false)
 
   useEffect(() => {
     const dateParam = date
       ? `date=${getDate(date!)}-${getMonth(date!) + 1}-${getYear(date!)}`
       : ''
 
-    if (dateParam) {
-      axios
-        .get(
-          `https://api.coingecko.com/api/v3/coins/bitcoin/history?${dateParam}`
-        )
-        .then((res) => {
-          setBtc(res.data)
-        })
-      axios
-        .get(
-          `https://api.coingecko.com/api/v3/coins/ethereum/history?${dateParam}`
-        )
-        .then((res) => {
-          setEth(res.data)
-        })
-    } else {
-      setBtc(null)
-      setEth(null)
+    const getCrypto = async () => {
+      if (dateParam) {
+        await axios
+          .get(
+            `https://api.coingecko.com/api/v3/coins/bitcoin/history?${dateParam}`
+          )
+          .then((res) => {
+            setBtc(res.data)
+          })
+        await axios
+          .get(
+            `https://api.coingecko.com/api/v3/coins/ethereum/history?${dateParam}`
+          )
+          .then((res) => {
+            setEth(res.data)
+          })
+      } else {
+        setBtc(null)
+        setEth(null)
+      }
     }
+    getCrypto()
   }, [date])
 
   return (
