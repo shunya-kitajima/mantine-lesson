dee8faefc1a1a288c373c73118ab3addacaa1b2b
shunya-kitajima <42736897+shunya-kitajima@users.noreply.github.com>
2022-10-21 13:20:01 +0900

    import modules

---
+++ pages/table.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import { Table, Container, Loader, Center } from '@mantine/core'
+import { ArrowUturnLeftIcon } from '@heroicons/react/20/solid'
+import { useQueryTodos } from '../hooks/useQueryTodos'
+import { Layout } from '../components/Layout'
+
+const table: React.FC = () => {
+  return <div></div>
+}
+
+export default table
