9ede1ff6d52000c2ea9e9936dbc569f1db1e9b9d
shunya-kitajima <42736897+shunya-kitajima@users.noreply.github.com>
2022-10-21 13:29:08 +0900

    display table

--- pages/table.tsx
+++ pages/table.tsx
@@ -6,10 +6,45 @@ import { useQueryTodos } from '../hooks/useQueryTodos'
 import { Layout } from '../components/Layout'
 
 const TableDemo: React.FC = () => {
-  const { data } = useQueryTodos()
+  const { data, status } = useQueryTodos()
+  const rows = data?.map((element) => (
+    <tr key={element.id}>
+      <td>{element.id}</td>
+      <td>{element.title}</td>
+      <td>{element.created_at}</td>
+    </tr>
+  ))
+
+  if (status === 'loading')
+    return (
+      <Layout title="Table">
+        <Center>
+          <Loader color="blue" size="lg" />
+        </Center>
+      </Layout>
+    )
 
   return (
     <Layout title="Table">
+      <Container>
+        <Table
+          striped
+          highlightOnHover
+          horizontalSpacing="lg"
+          verticalSpacing="sm"
+          captionSide="bottom"
+        >
+          <caption>Todo items from Supabase</caption>
+          <thead>
+            <tr>
+              <td>ID</td>
+              <td>Title</td>
+              <td>Created At</td>
+            </tr>
+          </thead>
+          <tbody>{rows}</tbody>
+        </Table>
+      </Container>
       <Center>
         <Link href="/">
           <ArrowUturnLeftIcon className="mt-4 h-6 w-6 cursor-pointer text-gray-300" />
