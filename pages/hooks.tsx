4ed201fb5c6f6a80a1cd5d637ad2724e243b811d
shunya-kitajima <42736897+shunya-kitajima@users.noreply.github.com>
2022-10-24 15:45:35 +0900

    display dialog and buttons

--- pages/hooks.tsx
+++ pages/hooks.tsx
@@ -1,9 +1,61 @@
-import React from 'react'
+import React, { useState } from 'react'
 import Link from 'next/link'
 import { ArrowUturnLeftIcon } from '@heroicons/react/20/solid'
+import { BrandGithub } from 'tabler-icons-react'
+import {
+  Dialog,
+  Group,
+  Button,
+  TextInput,
+  Text,
+  Avatar,
+  Indicator,
+  Paper,
+  Loader,
+  Code,
+  Progress,
+  Grid,
+  Center,
+} from '@mantine/core'
+import { useDisclosure } from '@mantine/hooks'
+import { Layout } from '../components/Layout'
 
 const HooksDemo: React.FC = () => {
-  return <div></div>
+  const [opened, handlers] = useDisclosure(false, {
+    onOpen: () => console.log('Opened'),
+    onClose: () => console.log('Closed'),
+  })
+
+  return (
+    <Layout title="Hooks">
+      <Dialog
+        opened={opened}
+        withCloseButton
+        onClose={() => handlers.close()}
+        size="lg"
+        radius="md"
+      >
+        <Text size="sm" mb="md">
+          Subscribe to email newsletter
+        </Text>
+        <Group>
+          <TextInput placeholder="xxx@gmail.com" className="flex-1" />
+          <Button onClick={() => handlers.close()}>Subscribe</Button>
+        </Group>
+      </Dialog>
+      <Group direction="column" position="center">
+        <Button compact onClick={() => handlers.toggle()}>
+          Toggle dialog
+        </Button>
+        <Button color="cyan" compact onClick={() => handlers.open()}>
+          Open dialog
+        </Button>
+        <Button color="orange" compact onClick={() => handlers.close()}>
+          Close dialog
+        </Button>
+      </Group>
+    </Layout>
+  )
 }
 
 export default HooksDemo
