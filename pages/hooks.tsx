6def95e3e3789f47c2fc9b5661ca0b9982b9ea59
shunya-kitajima <42736897+shunya-kitajima@users.noreply.github.com>
2022-10-24 15:31:24 +0900

    import modules

---
+++ pages/hooks.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+import Link from 'next/link'
+import { ArrowUturnLeftIcon } from '@heroicons/react/20/solid'
+
+const HooksDemo: React.FC = () => {
+  return <div></div>
+}
+
+export default HooksDemo
